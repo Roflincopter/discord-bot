2892d7fd38f0b3097c5286198f91c2ba80bc90c7
Dennis Brentjes <d.brentjes@gmail.com>
2020-05-05 23:10:29 +0200

    Awaits the send message call.

--- commands/kill.js
+++ commands/kill.js
@@ -1,8 +1,8 @@
 module.exports = {
 	name: 'kill',
 	description: 'kills the bot. Use when the bot is stuck, should come up again.',
-	execute(message) {
-		message.channel.send("Stopping myself, should be back soon.");
+	async execute(message) {
+		await message.channel.send("Stopping myself, should be back soon.");
 		process.exit(1);
 	},
 };
