1efefa42149a0a10600cd6ee11e9900a89916136
Dennis Brentjes <d.brentjes@gmail.com>
2020-05-05 20:31:10 +0200

    Says something when you execute the kill command

--- commands/kill.js
+++ commands/kill.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: 'kill',
 	description: 'kills the bot. Use when the bot is stuck, should come up again.',
 	execute(message) {
+		message.channel.send("Stopping myself, should be back soon.");
 		process.exit(1);
 	},
 };
